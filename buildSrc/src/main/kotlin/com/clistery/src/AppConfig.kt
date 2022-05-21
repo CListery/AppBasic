6e88be768a6406ca4d44123aeb3f6e261b6fd2c9
caiyuhong <cai1083088795@gmail.com>
2022-05-21 10:42:31 +0800

    init commit

---
+++ buildSrc/src/main/kotlin/com/clistery/src/AppConfig.kt
@@ -0,0 +1,13 @@
+package com.clistery.src
+
+object AppConfig {
+    const val compileSdk = 30
+    const val buildToolsVersion = "30.0.3"
+    const val minSdk = 14
+    const val targetSdk = 30
+    
+    const val GROUP_ID = "io.github.clistery"
+    const val ARTIFACT_ID = "appbasic"
+    const val versionName = "1.0.0"
+    const val versionCode = 1
+}
