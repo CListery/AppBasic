fdf02eed5ecad2aaaa6f90c8129cc75e29af04a2
caiyuhong <cai1083088795@gmail.com>
2022-09-16 16:43:23 +0800

    修改仓库

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -1,3 +1,17 @@
+
+pluginManagement {
+    repositories {
+        mavenCentral()
+        google()
+    }
+}
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        google()
+    }
+}
+
 files("../gradle/libs.versions.toml").also { catalogVersionsFile->
     if (true == catalogVersionsFile.singleOrNull()?.exists()) {
         enableFeaturePreview("VERSION_CATALOGS")
