f867a81c0124e28ac2aee8ce854ea335ee36b3ab
caiyuhong <cai1083088795@gmail.com>
2022-09-16 16:39:08 +0800

    修改仓库

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,19 @@ if (file("gradle/libs.versions.toml").exists()) {
     enableFeaturePreview("VERSION_CATALOGS")
 }
 
+pluginManagement {
+    repositories {
+        mavenCentral()
+        google()
+    }
+}
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        google()
+    }
+}
+
 rootProject.name = "AppBasic"
 include(":lib_appbasic")
 include(":libapp")
